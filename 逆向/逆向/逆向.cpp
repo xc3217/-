89985ce598440b6b7b82b026135f1bd708f1db4b
xc3217 <84855860+xc3217@users.noreply.github.com>
2023-01-16 09:31:09 +0800

    Add files via upload

---
+++ 逆向/逆向/逆向.cpp
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int fun(int x, int y) {
+	int temp=10;
+	int result = x > y ? x : y;
+	result += temp;
+	return result;
+}
+int main(int argc, char** argv) {
+	printf("fun(10, 20) = % d\n", fun(10, 20));
+}
